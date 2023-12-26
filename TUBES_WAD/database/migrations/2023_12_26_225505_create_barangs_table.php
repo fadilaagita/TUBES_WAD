fa3571bec7aa81f741591f01e0d0284dfd64c48c
Mufid Musyaffa <mufidmusyaffa@student.telkomuniversity.ac.id>
2023-12-27 06:02:08 +0700

    Update 2023_12_26_225505_create_barangs_table.php

--- TUBES_WAD/database/migrations/2023_12_26_225505_create_barangs_table.php
+++ TUBES_WAD/database/migrations/2023_12_26_225505_create_barangs_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('kategori_id')->constrained('categories', 'kategori_id')->onDelete('cascade');
+            $table->string('nama_barang');
             $table->decimal('harga', 10, 2);
+            $table->foreignId('kategori_id')->constrained('categories', 'kategori_id')->onDelete('cascade');
             $table->integer('stock');
             $table->string('deskripsi');
             $table->foreignId('supplier_id')->constrained('suppliers', 'supplier_id')->onDelete('cascade');
