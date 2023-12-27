ead364bee45ee32dc1fb27a27560001dfe60f3df
Mufid Musyaffa <mufidmusyaffa@student.telkomuniversity.ac.id>
2023-12-28 02:59:53 +0700

    Update sidebar.blade.php

--- TUBES_WAD/resources/views/layouts/sidebar.blade.php
+++ TUBES_WAD/resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,29 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<style>
+    .custom-navbar {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        z-index: 100;
+        background-color: #9A616D;
+    }
+    .custom-navbar .navbar-brand {
+        color: #ffffff;
+    }
+    .custom-navbar .navbar-nav .nav-link {
+        color: #ffffff;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg custom-navbar">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Manajemen-Gudang</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Features</a>
@@ -15,6 +31,11 @@
           <li class="nav-item">
             <a class="nav-link" href="#">Pricing</a>
           </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            <a class="nav-link" href="/logout">Logout</a>
+            </li>
         </ul>
       </div>
     </div>
