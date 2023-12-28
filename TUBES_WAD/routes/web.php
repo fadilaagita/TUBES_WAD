6f853ed2836357a84c1914923cd7cbc14fdf7327
Mufid Musyaffa <mufidmusyaffa@student.telkomuniversity.ac.id>
2023-12-28 16:00:57 +0700

    Update web.php

--- TUBES_WAD/routes/web.php
+++ TUBES_WAD/routes/web.php
@@ -27,7 +27,6 @@ Route::group(['middleware' => ['auth']], function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
     //crud karyawan
-<<<<<<< Updated upstream
     Route::get('/admin/karyawan', [KaryawanController::class, 'index'])->name('admin.karyawan.index');
     //crud supplier
     Route::get('/admin/supplier', [SupplierController::class, 'index'])->name('supplier.index');
@@ -36,7 +35,5 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/admin/supplier/{id}/edit', [SupplierController::class, 'edit'])->name('supplier.edit');
     Route::put('/admin/supplier/{id}', [SupplierController::class, 'update'])->name('supplier.update');
     Route::delete('/admin/supplier/{id}', [SupplierController::class, 'destroy'])->name('supplier.delete');
-=======
     Route::get('/admin/karyawan', [KaryawanController::class, 'index'])->name('karyawan.index');
->>>>>>> Stashed changes
 });
